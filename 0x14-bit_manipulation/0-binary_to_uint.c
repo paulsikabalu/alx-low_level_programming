551b80b6680c44b2a10324f93ca1309d716adf81
paulsikabalu <sikabalu@outlook.com>
2023-05-03 23:39:10 -0700

    0x14 bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,20 +9,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    if (b == NULL)
-        return 0;
+	unsigned int ui;
+	int len, base_two;
 
-    unsigned int ui = 0;
-    for (int i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return 0;
+	if (!b)
+		return (0);
 
-        ui <<= 1;
-        if (b[i] == '1')
-            ui += 1;
-    }
+	ui = 0;
 
-    return ui;
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
+
+	return (ui);
 }
-
