10ec49694fb7b32b0dba16ae2059246477336e66
sikabalupaul <4929210@gmail.com>
2023-08-31 19:48:42 +0300

    Makefile

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""The module for island_perimeter"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island"""
+    perimeter = 0
+    for y, row in enumerate(grid):
+        for x, cell in enumerate(row):
+            if cell == 1:
+                if y == 0 or grid[y - 1][x] == 0:
+                    perimeter += 1
+                if y == len(grid) - 1 or grid[y + 1][x] == 0:
+                    perimeter += 1
+                if x == 0 or grid[y][x - 1] == 0:
+                    perimeter += 1
+                if x == len(row) - 1 or grid[y][x + 1] == 0:
+                    perimeter += 1
+    return perimeter
